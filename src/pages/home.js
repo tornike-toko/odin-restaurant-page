3747e8b9a996e6bdf2479bbf6cfc3e36cae83a8f
tornike <tornike.chokheli0@gmail.com>
2025-04-30 19:46:41 +0400

    added images folder and foods images, in menu added function to add menu element, added style

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,12 +2,16 @@ const pageName = document.createElement("h1")
 pageName.innerText = "Home Page"
 const h2 = document.createElement("h2")
 h2.innerText = "experience the taste of TOKO's Restaurant"
+const desc = document.createElement("p")
+desc.innerText = `Step into a world of delightful aromas and exquisite tastes. At TOKO's Restaurant,
+we craft each dish with passion and the finest imaginary ingredients.`
 
 
 const homeCont = document.createElement("div")
 
 homeCont.append(pageName)
 homeCont.append(h2)
+homeCont.append(desc)
 
 
 export { homeCont }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,8 +1,43 @@
+import item_1 from "../foodImages/item1.png"
+import item_2 from "../foodImages/item2.png"
+import item_3 from "../foodImages/item3.png"
+import item_4 from "../foodImages/item4.png"
+import item_5 from "../foodImages/item5.png"
+
 const pageName = document.createElement("h1")
-pageName.innerText = "Menu Page"
+pageName.innerText = "Menu"
+
+const list = document.createElement("ul")
+
+function createEl(foodImg,foodName,foodDesc,foodPrice){
+    const el = document.createElement("li")
+    const img = document.createElement("img")
+    const wrapper = document.createElement("div")
+    const title = document.createElement("h3")
+    const desc = document.createElement("p")
+    const price = document.createElement("p")
+    
+    img.src = foodImg
+    title.innerText = foodName
+    desc.innerText = foodDesc
+    price.innerText = "$" + foodPrice
+    
+    wrapper.append(title)
+    wrapper.append(desc)
+    el.append(img)
+    el.append(wrapper)
+    el.append(price)
+    list.append(el)
+}
+
+createEl(item_1,"coffee","what else do you need", 15)
+createEl(item_2,"strawberry cacke","with srawberryes", 3)
+createEl(item_3,"strawberry cacke","with srawberryes", 30)
+createEl(item_4,"strawberry cacke","with srawberryes", 3)
+createEl(item_5,"strawberry cacke","with srawberryes", 3)
 
 const menuCont = document.createElement("div")
 
 menuCont.append(pageName)
-
+menuCont.append(list)
 export { menuCont }
